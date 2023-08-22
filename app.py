6b4e78e5daada2aed33619e339a8d872f30d00c1
krylis <cmcshann@gmail.com>
2023-08-21 17:11:20 -0700

    pulled cafe data from the database and displayed the list of names of the cafes on front page

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+
+db.init_app(app)
+
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    map_url = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String)
+    coffee_price = db.Column(db.String)
+
+
+@app.route('/')
+def index():
+    cafes = Cafe.query.all()
+    return render_template('index.html', cafes=cafes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
